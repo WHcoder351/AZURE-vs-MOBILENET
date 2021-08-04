761bd384c49b6529949cc4e844d2fd701ccf0b0a
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-04 14:20:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-Webcam.set({
-    width: 310,
-    height: 300,
-    image_format: 'png',
-    png_quality: "100",
-
-    constraints: {
-        facingMode: "environment"
-    }
-});
-
-camera = document.getElementById("camera");
-Webcam.attach(camera);
-function takeSnap(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("camera2").innerHTML = '<img src =" '+data_uri+' " id = "Cap" />';
-    })
-}
 console.log("ml5 version", ml5.version);
 classifier = ml5.imageClassifier('MobileNet', mL);
 function mL(){
     console.log("Model Loaded!");
 }
 
-function Check(){
+function ID(){
     img = document.getElementById("Cap");
     classifier.classify(img, gotResult);
 }
